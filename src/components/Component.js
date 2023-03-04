1ad65241ecce39e4e488e26afd1dcad7a84415ac
Digvijay Sethi <59555227+justxdv@users.noreply.github.com>
2023-03-04 16:25:27 +0530

    Update Component.js

--- src/components/Component.js
+++ src/components/Component.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 
 const Component = (props) => {
-    
-    const [error, setError] = useState(false);
+  const [error, setError] = useState(false);
+
+  const changeFunc = () => {
+    throw new Error("An error occurred.");
+  };
 
   return (
     <>
-    {    
-        error && <div>{somethingNotDefined}</div>
-    }
+      {error && <div>{somethingNotDefined}</div>}
+      <button onClick={changeFunc}>Click me to generate an error</button>
     </>
   );
 };
