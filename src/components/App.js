743c2b2600ed859f129c694d4630f51bd337fcb1
Digvijay Sethi <59555227+justxdv@users.noreply.github.com>
2023-03-06 17:58:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,20 @@ import React from "react";
 import "../styles/App.css";
 import Component from "./Component";
 import ErrorBoundary from "./ErrorBoundary";
-
 const App = () => {
-  const errorGenerator = () => {
-    // Trigger an error in the Component
-    const component = document.querySelector("#component");
-    component.click();
+  let throwErrorFunction;
+  let errorGenerator = () => {
+    throwErrorFunction(true);
   };
 
+  let changeFunc = (updateError) => {
+    throwErrorFunction = updateError;
+  };
   return (
     <div id="main">
       <h2>React Error Boundaries</h2>
       <ErrorBoundary>
-        <Component id="component" />
+        <Component change={changeFunc} />
       </ErrorBoundary>
       <button id="gen" onClick={errorGenerator}>
         Generate Error
